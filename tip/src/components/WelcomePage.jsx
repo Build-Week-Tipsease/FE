1b300da70581330fe5cef0aa88e668c84a05d34b
Ale <alearanibar@gmail.com>
2019-10-18 15:56:19 -0700

    new

---
+++ tip/src/components/WelcomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './WelcomePage.css'
+const WelcomePage = () => {
+
+
+    return(
+        <div>
+            <div className='tip-image'>
+            <h1>Hi</h1>
+            </div>
+            <div>
+                <p>Hello</p>
+            </div>
+        </div>
+    )
+}
+export default WelcomePage;
