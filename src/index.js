ffc70bccd33e1b39942f35fb4fcf712cf35ea9f6
Nurudeen <nurudeen.amedu@gmail.com>
2019-10-25 08:29:12 +0100

    redux dev tools removed

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ const store = createStore(
     {},
     compose(
         applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     )
 )
 
